4667122bb71fbe759e5bae511358c7f2746aec69
Alexandru Manta <alexandru.manta@ing.com>
2021-03-03 15:43:01 +0200

    chore: testimonials

--- src/app/components/layout/testimonials/testimonials.component.ts
+++ src/app/components/layout/testimonials/testimonials.component.ts
@@ -7,76 +7,11 @@ declare var $: any;
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
-  customOptions: OwlOptions = {
-    loop: true,
-    margin: 30,
-    nav: false,
-    smartSpeed: 3000,
-    autoplay: true,
-    navText: ['<span class="icon-Arrow-Left"></span>', '<span class="icon-Arrow-Right"></span>'],
-    responsive: {
-      0: {
-        items: 1
-      },
-      480: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      800: {
-        items: 1
-      },
-      1200: {
-        items: 1
-      }
-    }
-  }
-  video: any = false;
+ 
 
   constructor() { }
 
-  id = 'btfgGf2wDgI';
-  private player;
-  private ytEvent;
-
-
-
-
   ngOnInit(): void {
 
-    const tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(tag);
-
   }
-
-  playVideo() {
-    this.video = !this.video
-    // this.video.playVideo();
-  }
-
-  
-
-  ngAfterViewInit() {
-    // let playVideo: HTMLElement = document.querySelector('#play-video') as HTMLElement;
-    // let checkExist = setInterval(function () {
-    //   if (playVideo && playVideo instanceof HTMLElement) {
-
-    //     setTimeout(() => {
-    //       $('#play-video').on('click', function (ev) {
-
-    //         $("#video")[0].src += "&autoplay=1";
-    //         ev.preventDefault();
-
-    //       });
-    //     }, 3000);
-
-    //     clearInterval(checkExist);
-    //   }
-    // }, 100); // check every 100ms
-
-  }
-
 }
