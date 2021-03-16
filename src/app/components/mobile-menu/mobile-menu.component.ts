bf16d2fe6c73b485d896fb0fb8acc9c8295b54f1
Alexandru Manta <alexandru.manta@ing.com>
2021-03-16 13:16:33 +0200

    chore: mobile menu categories

--- src/app/components/mobile-menu/mobile-menu.component.ts
+++ src/app/components/mobile-menu/mobile-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { ProductsService } from 'src/app/pages/products/products.service';
 declare var $: any;
 @Component({
   selector: 'app-mobile-menu',
@@ -11,10 +12,15 @@ export class MobileMenuComponent implements OnInit {
   open: boolean;
   submenuOpen: boolean;
   navMenu: any;
+  categories: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private _ProductsService: ProductsService) { }
 
   ngOnInit(): void {
+
+    this._ProductsService.getCategories().then(data => {
+      this.categories = data.categories;
+    })
     
      this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
