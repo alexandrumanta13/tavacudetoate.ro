a755170829e9534ad0f4c407069c46c171db5ecb
Alexandru Manta <alexandru.manta@ing.com>
2021-05-26 17:57:39 +0300

    chore: auth/register tab form

--- api/app.js
+++ api/app.js
@@ -4,10 +4,18 @@ const app = express()
 const expressStaticGzip = require('express-static-gzip')
 const server = new http.Server(app)
 
-app.use('/', expressStaticGzip(__dirname + '/dist', {
+// app.use('/', expressStaticGzip(__dirname + '/dist', {
+//   enableBrotli: true
+// }))
+// app.use('/', express.static(__dirname + '/dist'))
+
+app.get('*.*', expressStaticGzip(join(DIST_FOLDER, 'browser'), {
   enableBrotli: true
 }))
-app.use('/', express.static(__dirname + '/dist'))
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser')))
+app.get('*', (req, res) => {
+    res.render(join(DIST_FOLDER, 'browser', 'index.html'), {req, res})
+})
 
 require('zone.js/dist/zone-node')
 require('reflect-metadata')
