47f973683491b3e8f3ee3f5dd445d6c3f39c000a
Alexandru Manta <alexandru.manta@ing.com>
2021-05-26 17:55:45 +0300

    chore: auth/register tab form

--- server.ts
+++ server.ts
@@ -14,7 +14,8 @@ global['localStorage'] = localStorage;
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/tavacudetoate/browser');
+  //const distFolder = join(process.cwd(), 'dist/tavacudetoate/browser');
+  const distFolder = join(process.cwd());
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
